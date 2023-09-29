bdfd702eabf5a191156f77398b45343d73e68375
unknown <shazaniyu@gmail.com>
2023-09-29 04:48:40 -0700

    contact adress

--- pages/Academy.js
+++ pages/Academy.js
@@ -137,7 +137,7 @@ function Academy(){
         Contact Address
       </label>
       <input 
-      className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      className="mt-4 shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
       id="grid-last-name" 
       defaultValue={formData.contact}
       onChange={handleInputChange}
